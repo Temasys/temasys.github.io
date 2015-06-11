394926bf104acc6eb030041307a0a416ebf3129e
Thomas Gorissen <serrynaimo@me.com>
2015-06-11 18:17:51 +0800

    fix

--- resources/js/screenshare.js
+++ resources/js/screenshare.js
@@ -1,11 +1,17 @@
-var skylink = new Skylink();
+function shareCamera() {
+    navigator.getUserMedia({video: true}, function(stream) {
+        var v = document.getElementById('screen');
+        attachMediaStream(v, stream);
+    }, function(error) {
 
-skylink.setLogLevel(skylink.LOG_LEVEL.DEBUG);
+    });
+}
 
-skylink.on("mediaAccessSuccess", function(stream) {
+function shareScreen() {
+    navigator.getUserMedia({video: {mediaSource: 'window'}}, function(stream) {
+        var v = document.getElementById('screen');
+        attachMediaStream(v, stream);
+    }, function(error) {
 
-    var v = document.getElementById('screen');
-    attachMediaStream(v, stream);
-
-});
-
+    });
+}
