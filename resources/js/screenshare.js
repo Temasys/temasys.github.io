537638331099b71669407a7708a7ebd98e92c412
Thomas Gorissen <serrynaimo@me.com>
2015-08-18 15:15:44 +0800

    js update

--- resources/js/screenshare.js
+++ resources/js/screenshare.js
@@ -8,6 +8,12 @@ function shareCamera() {
 }
 
 function shareScreen() {
+    if(location.protocol === 'http:') {
+        if(window.confirm('To use screensharing you\'ll have to visit the secure HTTPS version of this site.\nWould you like to go there now?')) {
+            location = 'https://' + location.host + location.pathname;
+        }
+        return;
+    }
     navigator.getUserMedia({video: {mediaSource: 'window'}}, function(stream) {
         var v = document.getElementById('screen');
         attachMediaStream(v, stream);
